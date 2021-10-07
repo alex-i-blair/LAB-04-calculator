9bcc1543fb9f42e0fe0ff0d5e1b5524bb5af713b
Alex Blair <bclan11@gmail.com>
2021-10-07 11:21:38 -0700

    Calc

--- test/tests.js
+++ test/tests.js
@@ -1,11 +1,9 @@
 // IMPORT MODULES under test here:
-// import { add } from '../calculator.js';
+import { add } from '../calculations.js';
 
 const test = QUnit.test;
 
-function add(num1, num2) {
-    return num1 + num2;
-}
+
 // name your test by what it is testing
 test('test 5 + 3', (expect) => {
     //Arrange
