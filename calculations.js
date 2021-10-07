94c45167d368ed5cecbfb666d457ecdde16610dc
Alex Blair <bclan11@gmail.com>
2021-10-07 13:57:46 -0700

    idk

--- calculations.js
+++ calculations.js
@@ -2,3 +2,15 @@
 export function add(num1, num2) {
     return num1 + num2;
 }
+export function multiply(num1, num2) {
+    return num1 * num2;
+}
+export function subtract(num1, num2) {
+    return num1 - num2;
+}
+export function exponent(num1,num2) {
+    return num1 ** num2;
+}
+export function division(num1, num2) {
+    return num1 / num2;
+}
