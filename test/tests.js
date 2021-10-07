e242dfefa834b52a6a36826fbdd929562233992c
Alex Blair <bclan11@gmail.com>
2021-10-07 11:15:59 -0700

    add tests

--- test/tests.js
+++ test/tests.js
@@ -3,15 +3,36 @@
 
 const test = QUnit.test;
 
+function add(num1, num2) {
+    return num1 + num2;
+}
 // name your test by what it is testing
-test('time to test a function', (expect) => {
+test('test 5 + 3', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const x = 5;
+    const y = 3;
+    const expected = 8;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true; // use your function here
+    const actual = add(x, y); // use your function here
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('test 4+3', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const x = 4;
+    const y = 3;
+    const expected = 7;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = add(x, y); // use your function here
 
     //Expect
     // Make assertions about what is expected versus the actual result
