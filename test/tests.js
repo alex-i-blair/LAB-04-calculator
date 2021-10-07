4dce99c11216519d0ac329ec947d6152009ca684
Alex Blair <bclan11@gmail.com>
2021-10-07 14:07:57 -0700

    tests

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,9 @@
 // IMPORT MODULES under test here:
 import { add } from '../calculations.js';
+import { subtract } from '../calculations.js';
+import { multiply } from '../calculations.js';
+import { division } from '../calculations.js';
+import { exponent } from '../calculations.js';
 
 const test = QUnit.test;
 
@@ -21,16 +25,62 @@ test('test 5 + 3', (expect) => {
     expect.equal(actual, expected);
 });
 
-test('test 4+3', (expect) => {
+test('test math 3-4', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const x = 4;
-    const y = 3;
-    const expected = 7;
+    const x = 3;
+    const y = 4;
+    const expected = -1;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = add(x, y); // use your function here
+    const actual = subtract(x, y); // use your function here
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+test('test math 3*4', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const x = 3;
+    const y = 4;
+    const expected = 12;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = multiply(x, y); // use your function here
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+test('test math 6/2', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const x = 6;
+    const y = 2;
+    const expected = 3;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = division(x, y); // use your function here
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('test math 2^4', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const x = 2;
+    const y = 4;
+    const expected = 16;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = exponent(x, y); // use your function here
 
     //Expect
     // Make assertions about what is expected versus the actual result
